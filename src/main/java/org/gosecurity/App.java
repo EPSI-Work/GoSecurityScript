56c66190ca3802551ed2c06561df97503ab3054a
Baptiste <baptiste.lecat44@gmail.com>
2022-01-26 17:12:19 +0100

    Implement Tool file fetcher

--- src/main/java/org/gosecurity/App.java
+++ src/main/java/org/gosecurity/App.java
@@ -1,5 +1,11 @@
 package org.gosecurity;
 
+import org.gosecurity.src.model.Tool;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,27 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        getListTools("C:\\Users\\TechnoCraft\\Documents\\Dev\\EPSI\\JAVA\\GoSecurity\\liste.txt");
+    }
+
+    public static List<Tool> getListTools(String pathFile){
+        List<Tool> listTools = new ArrayList<>();
+        try {
+            File myObj = new File(pathFile);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+
+                String[] listElement = data.split("\\t");
+
+                Tool tool = new Tool(listElement[0], listElement[1]);
+                listTools.add(tool);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return listTools;
     }
 }
