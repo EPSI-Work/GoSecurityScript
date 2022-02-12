60597d2ff5d4da40365e0b8f4fd16a1f4f934afe
Lecat Baptiste <60200125+BaptisteLecat@users.noreply.github.com>
2022-02-12 12:23:16 +0100

    Build

--- src/main/java/org/gosecurity/generator/AgentGenerator.java
+++ src/main/java/org/gosecurity/generator/AgentGenerator.java
@@ -63,8 +63,8 @@ public class AgentGenerator {
 
             indexFile.createNewFile();
             Path sourceJPGFile = Paths.get(basePath + this.agent.getId() + ".jpg");
-            Path copiedJPGFile = Paths.get(this.websitePath);
-            Files.copy(sourceJPGFile, copiedJPGFile, StandardCopyOption.REPLACE_EXISTING);
+            Path copiedJPGFile = Paths.get(this.websitePath + "/img");
+            Files.copy(sourceJPGFile, copiedJPGFile);
             Files.write(indexFile.toPath(), content.getBytes(StandardCharsets.UTF_8));
             System.out.println("File created");
         } catch (IOException e) {
